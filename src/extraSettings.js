51523e363a535546a1af80a4db61fad83d625689
kathrynkaren <69753517+kathrynkaren@users.noreply.github.com>
2021-01-31 22:37:35 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -31,7 +31,7 @@ let fruitSlowDown = false;
 let adLink = 'https://twitter.com/cherryvguk';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '柾泰 tingtingball';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
